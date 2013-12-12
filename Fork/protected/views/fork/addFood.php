8aa58a2d83e82716cf25a789253d8a2c324f5697
hackedhacker <jakks_team@yahoo.com>
2013-12-12 22:01:36 +0700

    added new add page instead of Pop page

---
+++ Fork/protected/views/fork/addFood.php
@@ -0,0 +1,110 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+<!--
+<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<p>Congratulations! You have successfully created your Yii application.</p>
+
+<p>You may change the content of this page by modifying the following two files:</p>
+<ul>
+	<li>View file: <code><?php echo __FILE__; ?></code></li>
+	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+</ul>
+
+<p>For more details on how to further develop this application, please read
+the <a href="http://www.yiiframework.com/doc/">documentation</a>.
+Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
+should you have any questions.</p>
+-->
+
+<div id="contentHeader">
+	<h1><?php echo Yii::app()->name; ?> - Forks</h1>
+</div>
+
+<div class="container">
+	<div class="grid-24">				
+		<div class="widget">
+			<div class="widget-header">
+				<h3><?php
+				if($id<1)
+					echo "Add Food";
+				else
+					echo "Edit Food";
+				?></h3>
+			</div>
+			<div class="widget-content">
+				<form method="post" class="form uniformForm validateForm">
+					<?php
+					if($id>=1)
+					{
+					?>
+					<input type="hidden" name="food_id" id="food_id" value="<?=$id?>" />
+					<?php
+					}
+					?>
+					<div class="field-group">
+						<label for="restaurant">Restaurant Name:</label>
+						<div class="field">
+							<select name="restaurant" id="restaurant">
+								<?php
+									foreach($restaurantList as $restaurant)
+									{
+								?>
+								<option value="<?=$restaurant['restaurant_id']?>" <?php if($data!=null && $data['restaurant_name']==$restaurant['restaurant_name']) echo "selected=\"selected\""; ?> ><?=$restaurant['restaurant_name']?></option>
+								<?php
+									}
+								?>
+							</select>
+						</div>	
+					</div>
+					<div class="field-group">
+						<label for="food_cat">Food Category:</label>
+						<div class="field">
+							<select name="food_cat" id="food_cat">
+								<?php
+									foreach($foodCatList as $foodCat)
+									{
+								?>
+								<option value="<?=$foodCat['food_category_id']?>" <?php if($data!=null && $data['food_category_name']==$foodCat['food_category_name']) echo "selected=\"selected\""; ?> ><?=$foodCat['food_category_name']?></option>
+								<?php
+									}
+								?>
+							</select>
+						</div>	
+					</div>
+					<div class="field-group">
+						<label for="food_name">Food Name:</label>
+						<div class="field">
+							<input type="text" name="food_name" id="food_name" size="20" class="validate[required]" value="<?php if($data!=null) echo $data['food_name']; ?>" />	
+						</div>
+					</div>
+					<div class="field-group">
+						<label for="description">Food Description:</label>
+						<div class="field">
+							<textarea name="description" id="description" rows="5" cols="50"><?php if($data!=null) echo $data['description']; ?></textarea>
+						</div>	
+					</div>
+					<div class="field-group inlineField">	
+						<label for="food_img">Food Image:</label>
+						<div class="field">
+							<input type="file" name="food_img" id="food_img" value="<?php if($data!=null) echo $data['image']; ?>" />
+						</div>	
+					</div>
+					<div class="field-group inlineField">
+						<label for="price">Price:</label>
+						<div class="field">
+							<input type="text" name="price" id="price" size="8" class="validate[required,min[1000],max[1000000]]" value="<?php if($data!=null) echo $data['price']; ?>" />
+						</div>
+					</div>
+					<div class="actions">						
+						<button type="submit" class="btn btn-primary">Submit</button>
+						<button type="reset" class="btn btn-error">Reset</button>
+					</div>
+				</form>
+			</div>
+		</div>					
+	</div>
+</div>
---
+++ Fork/protected/views/fork/food.php
@@ -0,0 +1,89 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+<!--
+<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<p>Congratulations! You have successfully created your Yii application.</p>
+
+<p>You may change the content of this page by modifying the following two files:</p>
+<ul>
+	<li>View file: <code><?php echo __FILE__; ?></code></li>
+	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+</ul>
+
+<p>For more details on how to further develop this application, please read
+the <a href="http://www.yiiframework.com/doc/">documentation</a>.
+Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
+should you have any questions.</p>
+-->
+
+<div id="contentHeader">
+	<h1><?php echo Yii::app()->name; ?> - Forks</h1>
+</div>
+<?php
+	if(isset($success)&&($success==1||$success==2))
+	{
+?>
+<script>
+	$.alert ({ 
+		type: 'ok'
+		, title: '<?=$success=='1'?'Insert Success':'Update Success'?>'
+		, callback: function () { }	
+	});	
+</script>
+<?php
+	}
+?>
+<div class="container">
+	<div class="row">
+		<div class="grid-24">				
+			<div class="widget widget-table">
+				<div class="widget-header">
+					<h3>Food</h3>
+					<a href="index.php?r=fork/addFood">
+						<button type="submit" class="btn btn-primary btn-header"><span class="icon-plus-alt"></span>Add</button>
+					</a>
+				</div>
+				<div class="widget-content">
+					<table class="table table-bordered table-striped data-table">
+						<thead>
+							<tr>
+								<th>Restaurant Name</th>
+								<th>Food Category</th>
+								<th>Food Name</th>
+								<th>Description</th>
+								<th>Image</th>
+								<th>Price</th>
+								<th>Modify</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+								foreach($foodList as $food)
+								{
+							?>
+							<tr class="gradeA">
+								<td><?=$food['restaurant_name']?></td>
+								<td><?=$food['food_category_name']?></td>
+								<td><?=$food['food_name']?></td>
+								<td><?=$food['description']?></td>
+								<td><img src="<?=$food['image']?>" alt="<?=$food['food_name']?>" width="200px" height="200px" /></td>
+								<td><?=$food['price']?></td>
+								<td>
+									<a href="?r=fork/addFood&id=<?=$food['restaurant_food_id']?>"><button class="btn btn-gray"><span class="icon-pen"></span></button></a>
+									<button class="btn btn-red"><span class="icon-trash-fill"></span></button>
+								</td>
+							</tr>
+							<?php
+								}
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>					
+		</div>
+	</div>
+</div>
