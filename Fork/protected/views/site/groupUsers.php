a87f215cf09c52b7cef96679759674e0de173bf8
hackedhacker <jakks_team@yahoo.com>
2013-12-16 20:07:03 +0700

    Group User Page revised

--- Fork/protected/views/site/groupUsers.php
+++ Fork/protected/views/site/groupUsers.php
@@ -32,64 +32,183 @@ should you have any questions.</p>
 					<h3>Group Users</h3>
 				</div>
 				<div class="widget-content">
-					<form class="form uniformForm">
+					<form method="post" class="form uniformForm">
 						<p>
 							<div class="field-group">
-								<label>Application:</label>
+								<label for="application_id">Application:</label>
 								<div class="field">
-									<select name="cardtype" id="cardtype">
-										<option>Fork</option>
+									<select name="application_id" id="application_id">
+										<option <?php if($application_id==0) { ?>selected="selected" <?php }?> value="0" >Please Select An Application</option>
+										<?php
+											foreach($applicationList as $application)
+											{
+										?>
+										<option value="<?=$application['application_id']?>" <?php if($application_id===$application['application_id']) echo "selected=\"selected\""; ?> ><?=$application['application_name']?></option>
+										<?php
+											}
+										?>
 									</select>
 								</div>
 							</div>
 							<div class="field-group">
-								<label>Group:</label>
+								<label for="group_id">Group:</label>
 								<div class="field">
-									<select name="cardtype" id="cardtype">
-										<option>Admin</option>
+									<select name="group_id" id="group_id">
+									<?php
+										if($groupList==0)
+										{
+									?>
+										<option selected="selected">Please Select An Application First</option>
+									<?php
+										}
+										else
+										{
+											foreach($groupList as $group)
+											{
+									?>
+										<option <?php if($group_id===$group['group_id']) {?>selected="selected" <?php } ?>value="<?=$group['group_id']?>"><?=$group['group_name']?></option>
+									<?php
+											}
+										}
+									?>
 									</select>
 								</div>
 							</div>
 							<div class="actions">						
-								<button type="button" class="btn btn-primary"><span class="icon-users"></span>Search</button>
-							</div>						
+								<button type="submit" class="btn btn-primary"><span class="icon-users"></span>Search</button>
+							</div>
 						</p>
 					</form>
-					<div class="grid-10">
-						<select id="box1View" multiple="multiple" class="multiple" style="height:300px; width:100%">
-							<option value="501649">2008-2009 "Mini" Baja</option>
-							<option value="501497">AAPA - Asian American Psychological Association</option>
-							<option value="501053">Academy of Film Geeks</option>
-							<option value="500001">Accounting Association</option>
-							<option value="501227">ACLU</option><option value="501610">Active Minds</option>
-							<option value="501514">Activism with A Reel Edge (A.W.A.R.E.)</option>
-							<option value="501656">Adopt a Grandparent Program</option>
-							<option value="501050">Africa Awareness Student Organization</option>
-							<option value="501075">African Diasporic Cultural RC Interns</option>
-							<option value="501493">Agape</option>
-							<option value="501562">AGE-Alliance for Graduate Excellence</option>
-							<option value="500676">AICHE (American Inst of Chemical Engineers)</option>
-							<option value="501460">AIDS Sensitivity Awareness Project ASAP</option>
-							<option value="500004">Aikido Club</option>
-							<option value="500336">Akanke</option>
-							<option value="500336">Akanke</option>
-							<option value="500336">Akanke</option>
-							<option value="500336">Akanke</option>
-							<option value="500336">Akanke</option>
-						</select>
-					</div>
-					<div class="grid-4">
-						<div class="dualControl" style="text-align:center;margin:125px auto 125px auto">
-							<button id="to2" type="button" class="btn mr5 mb15">&nbsp;&gt;&nbsp;</button>
-							<button id="to1" type="button" class="btn mr5">&nbsp;&lt;&nbsp;</button>
+					<form method="post" >
+						<input type="hidden" name="group_id_new" id="group_id_new" value=""/>
+						<input type="hidden" name="action" id="action" value="" />
+						<div class="grid-10">
+							<div style="text-align:center;margin:5px auto 5px auto">
+								Users Not In Group
+							</div>
+							<select name="notActive[]" id="notActive" multiple="multiple" class="multiple" style="height:300px; width:100%">
+							<?php
+								if($userOut!=null)
+								{
+									foreach($userOut as $user)
+									{
+							?>
+								<option value="<?=$user['user_id']?>" ><?=$user['username']?></option>
+							<?php
+									}
+								}
+							?>
+							</select>
 						</div>
-					</div>
-					<div class="grid-10">
-						<select id="box1View" multiple="multiple" class="multiple" style="height:300px; width:100%">
-						</select>
-					</div>
+						<div class="grid-4">
+							<div class="dualControl" style="text-align:center;margin:125px auto 125px auto">
+								<button id="add" type="button" class="btn mr5 mb15">&nbsp;&gt;&nbsp;</button>
+								<button id="remove" type="button" class="btn mr5">&nbsp;&lt;&nbsp;</button>
+							</div>
+						</div>
+						<div class="grid-10">
+							<div style="text-align:center;margin:5px auto 5px auto">
+								User In Group
+							</div>
+							<select name="active[]" id="active" multiple="multiple" class="multiple" style="height:300px; width:100%">
+							<?php
+								if($userIn!=null)
+								{
+									foreach($userIn as $user)
+									{
+							?>
+								<option value="<?=$user['user_id']?>" ><?=$user['username']?></option>
+							<?php
+									}
+								}
+							?>
+							</select>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
+
+<script>
+$('#remove').live ('click', function (e) {
+	$('#group_id_new').val($('#group_id').val());
+	$('#action').val('remove');
+	$.ajax({
+		url:'<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=site/addGroupUsers',
+		type:'post',
+		data: $(this.form).serialize(),
+		success: function(data){
+			$.ajax({
+				url:'<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=site/groupList&action=in&app='+$('#application_id').val()+'&group='+$('#group_id').val(),
+				type:'get',
+				success: function(data){
+					$('#active').html(data);
+				}
+			});
+			$.ajax({
+				url:'<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=site/groupList&action=out&app='+$('#application_id').val()+'&group='+$('#group_id').val(),
+				type:'get',
+				success: function(data){
+					$('#notActive').html(data);
+				}
+			});
+		}
+	});
+	//$(this.form).submit();
+});
+$('#add').live ('click', function (e) {
+	$('#group_id_new').val($('#group_id').val());
+	$('#action').val('add');
+	$.ajax({
+		url:'<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=site/addGroupUsers',
+		type:'post',
+		data: $(this.form).serialize(),
+		success: function(data){
+			$.ajax({
+				url:'<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=site/groupList&action=in&app='+$('#application_id').val()+'&group='+$('#group_id').val(),
+				type:'get',
+				success: function(data){
+					$('#active').html(data);
+				}
+			});
+			$.ajax({
+				url:'<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=site/groupList&action=out&app='+$('#application_id').val()+'&group='+$('#group_id').val(),
+				type:'get',
+				success: function(data){
+					$('#notActive').html(data);
+				}
+			});
+		}
+	});
+	//$(this.form).submit();
+});
+$('#application_id').change(function(){
+	$.ajax({
+		url:'<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=site/groupList&id='+$('#application_id').val(),
+		type:'get',
+		success: function(data){
+			$('#group_id').html(data);
+		}
+	});
+});
+/*
+$('#group_id').change(function(){
+	$.ajax({
+		url:'<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=site/groupList&action=in&app='+$('#application_id').val()+'&group='+$('#group_id').val(),
+		type:'get',
+		success: function(data){
+			$('#active').html(data);
+		}
+	});
+	$.ajax({
+		url:'<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=site/groupList&action=out&app='+$('#application_id').val()+'&group='+$('#group_id').val(),
+		type:'get',
+		success: function(data){
+			$('#notActive').html(data);
+		}
+	});
+});
+*/
+</script>
