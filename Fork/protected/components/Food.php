f320e8e200e4ff5695fbf0501f317f2376f4e10a
hackedhacker <jakks_team@yahoo.com>
2013-12-16 20:02:33 +0700

    added image upload feature

--- Fork/protected/components/Food.php
+++ Fork/protected/components/Food.php
@@ -34,19 +34,70 @@ class Food
 		$data = $foodList[0];
 		return new Food($data['restaurant_food_id'], $data['restaurant_id'], $data['food_category_id'], $data['food_name'], $data['description'], $data['image'], $data['price']);
 	}
+	private function generateRandomFileName()
+	{
+		$randomName = "";
+		for($i = 0;$i<15;$i++) {
+			$temp = rand()%62;
+			if($temp<10)
+				$randomName .= $temp;
+			else if($temp<36)
+				$randomName .= chr(($temp-10)+97);
+			else
+				$randomName .= chr(($temp-36)+65);
+		}
+		return $randomName;
+	}
 	public function insert()
 	{
-		$connection = Yii::app()->db;
-		$command = $connection->createCommand("INSERT INTO restaurantfood(restaurant_id, food_category_id, food_name, description, image, price, stsrc, userchange, datechange)  VALUES('".$this->restaurant_id."','".$this->food_category_id."','".$this->food_name."','".$this->description."','".$this->image."','".$this->price."','a','".Yii::app()->user->name."',NOW())");
-		return $command->execute();
+		$temp = explode(".", $this->image->name);
+		$extension = ".".end($temp);
+		$newFileName = '';
+		$imageUpload = 0;
+		if($this->image->name!=='')
+		{
+			$newFileName = $this->generateRandomFileName();
+			if($this->image->saveAs("images/foods/".$newFileName.$extension))
+			{
+				$imageUpload = 1;
+			}
+		}
+		if($imageUpload==1)
+		{
+			$connection = Yii::app()->db;
+			$command = $connection->createCommand("INSERT INTO restaurantfood(restaurant_id, food_category_id, food_name, description, image, price, stsrc, userchange, datechange)  VALUES('".$this->restaurant_id."','".$this->food_category_id."','".$this->food_name."','".$this->description."','".$newFileName.$extension."','".$this->price."','a','".Yii::app()->user->name."',NOW())");
+			return $command->execute();
+		}
+		else
+			return 0;
 	}
 	public function update($restaurant_id, $food_category_id, $food_name, $description, $image, $price)
 	{
-		if($this->restaurant_id!==$restaurant_id || $this->food_category_id!==$food_category_id || $this->food_name!==$food_name || $this->description!==$description || $this->image!==$image || $this->price!==$price)
+		if($this->restaurant_id!==$restaurant_id || $this->food_category_id!==$food_category_id || $this->food_name!==$food_name || $this->description!==$description || $image->name!=='' || $this->price!==$price)
 		{
-			$connection = Yii::app()->db;
-			$command = $connection->createCommand("UPDATE restaurantfood SET restaurant_id='".$restaurant_id."', food_category_id='".$food_category_id."', food_name='".$food_name."', description='".$description."', image='".$image."', price='".$price."', userchange='".Yii::app()->user->name."', datechange=NOW() WHERE stsrc='a' AND restaurant_food_id='".$this->restaurant_food_id."'");
-			return $command->execute();
+			$temp = explode(".", $image->name);
+			$extension = ".".end($temp);
+			$imageUpload = 1;
+			$updateOther = 1;
+			if($image->name!=='')
+			{
+				$imageUpload = 0;
+				if($image->saveAs("images/foods/".$this->image))
+				{
+					$imageUpload = 1;
+				}
+			}
+			if($this->restaurant_id!==$restaurant_id || $this->food_category_id!==$food_category_id || $this->food_name!==$food_name || $this->description!==$description || $this->price!==$price)
+			{
+				$updateOther = 0;
+				$connection = Yii::app()->db;
+				$command = $connection->createCommand("UPDATE restaurantfood SET restaurant_id='".$restaurant_id."', food_category_id='".$food_category_id."', food_name='".$food_name."', description='".$description."', price='".$price."', userchange='".Yii::app()->user->name."', datechange=NOW() WHERE stsrc='a' AND restaurant_food_id='".$this->restaurant_food_id."'");
+				$updateOther = $command->execute();
+			}
+			if($imageUpload==1 && $updateOther==1)
+				return 1;
+			else
+				return 0;
 		}
 		return 0;
 	}
--- Fork/protected/components/Provider.php
+++ Fork/protected/components/Provider.php
@@ -26,19 +26,70 @@ class Provider
 		$data = $providerList[0];
 		return new Provider($data['provider_id'],$data['provider_name'],$data['provider_image']);
 	}
+	private function generateRandomFileName()
+	{
+		$randomName = "";
+		for($i = 0;$i<15;$i++) {
+			$temp = rand()%62;
+			if($temp<10)
+				$randomName .= $temp;
+			else if($temp<36)
+				$randomName .= chr(($temp-10)+97);
+			else
+				$randomName .= chr(($temp-36)+65);
+		}
+		return $randomName;
+	}
 	public function insert()
 	{
-		$connection = Yii::app()->db;
-		$command = $connection->createCommand("INSERT INTO provider(provider_name, provider_image, stsrc, userchange, datechange) VALUES('".$this->provider_name."','".$this->provider_image."','a','".Yii::app()->user->name."',NOW())");
-		return $command->execute();
+		$temp = explode(".", $this->provider_image->name);
+		$extension = ".".end($temp);
+		$newFileName = '';
+		$imageUpload = 0;
+		if($this->provider_image->name!=='')
+		{
+			$newFileName = $this->generateRandomFileName();
+			if($this->provider_image->saveAs("images/providers/".$newFileName.$extension))
+			{
+				$imageUpload = 1;
+			}
+		}
+		if($imageUpload==1)
+		{
+			$connection = Yii::app()->db;
+			$command = $connection->createCommand("INSERT INTO provider(provider_name, provider_image, stsrc, userchange, datechange) VALUES('".$this->provider_name."','".$newFileName.$extension."','a','".Yii::app()->user->name."',NOW())");
+			return $command->execute();
+		}
+		else
+			return 0;
 	}
 	public function update($provider_name, $provider_image)
 	{
-		if($this->provider_name!==$provider_name || $this->provider_image!==$provider_image)
+		if($this->provider_name!==$provider_name || $provider_image->name!=='')
 		{
-			$connection = Yii::app()->db;
-			$command = $connection->createCommand("UPDATE provider SET provider_name='".$this->provider_name."', provider_image='".$this->provider_image."', userchange='".Yii::app()->user->name."', datechange=NOW() WHERE stsrc='a' AND provider_id='".$this->provider_id."'");
-			return $command->execute();
+			$temp = explode(".", $provider_image->name);
+			$extension = ".".end($temp);
+			$imageUpload = 1;
+			$nameChange = 1;
+			if($provider_image->name!=='')
+			{
+				$imageUpload = 0;
+				if($provider_image->saveAs("images/providers/".$this->provider_image))
+				{
+					$imageUpload = 1;
+				}
+			}
+			if($this->provider_name!==$provider_name)
+			{
+				$nameChange = 0;
+				$connection = Yii::app()->db;
+				$command = $connection->createCommand("UPDATE provider SET provider_name='".$this->provider_name."', userchange='".Yii::app()->user->name."', datechange=NOW() WHERE stsrc='a' AND provider_id='".$this->provider_id."'");
+				$nameChange = $command->execute();
+			}
+			if($imageUpload==1 && $nameChange==1)
+				return 1;
+			else
+				return 0;
 		}
 		return 0;
 	}
